2ed847691858295467bd65669281a6f96399af2d
Admin <Admin@PC1>
2025-01-29 20:05:06 +0800

    asd

--- TasteTracker/Models/RecipeItem.cs
+++ TasteTracker/Models/RecipeItem.cs
@@ -6,9 +6,10 @@ namespace TasteTracker.Models
     {
         public int Id { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Only letters are allowed.")]
         public string Name { get; set; } = "";
         [Required]
-
+    
         public string Description { get; set; } = "";
         [Required]
 
